24033396c7d69f06cb0f6b763c534cfa2af27a25
Marc Püls <mpuels@gmail.com>
2014-09-03 19:55:37 +0200

    Check in trayer-start.bash, if trayer is already running.

--- trayer-start.bash
+++ trayer-start.bash
@@ -1,18 +1,66 @@
 #!/bin/bash
 
-# TODO: Check, if trayer is already running.
-trayer \
-    --edge top \
-    --align right \
-    --SetDockType true \
-    --SetPartialStrut true \
-    --expand true \
-    --width 10 \
-    --transparent true \
-    --tint 0x191970 \
-    --height 12 &
+function main
+{
+    if [ "$(trayerIsRunning)" == false ]; then
+        echo starting trayer
+        start_trayer
+    fi
 
-dropbox start
+    if [ "$(nmappletIsRunning)" == false ]; then
+        echo starting nm-applet
+        start_nmapplet
+    fi
 
-# TODO: Check, if nm-applet is already running.
-#nm-applet --sm-disable &
+    start_dropbox
+}
+
+function trayerIsRunning
+{
+    daemonIsRunning trayer
+}
+
+function start_trayer
+{
+    trayer \
+        --edge top \
+        --align right \
+        --SetDockType true \
+        --SetPartialStrut true \
+        --expand true \
+        --width 10 \
+        --transparent true \
+        --tint 0x191970 \
+        --height 12 &
+}
+
+function start_dropbox
+{
+    dropbox start
+}
+
+function nmappletIsRunning
+{
+    daemonIsRunning nm-applet
+}
+
+function start_nmapplet
+{
+    nm-applet --sm-disable &
+}
+
+function daemonIsRunning
+{
+    local progName=$1
+    local nInstances=$(ps -C $progName \
+        | grep -v "PID TTY" \
+        | wc -l)
+
+    if [ $nInstances -gt 0 ]; then
+        echo true
+    else
+        echo false
+    fi
+}
+
+main
